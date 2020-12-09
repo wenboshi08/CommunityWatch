3414cda2eb142d0f470ec07d99d914de4a3e2541
Stuti Vishwabhan <svishwab@mit.edu>
2020-12-08 20:49:23 -0500

    added color markers back

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,284 +1,262 @@
 <template>
-    <div class="map col-md-8">
-        <div>
-            <div v-if="success" class="success-message">
-                {{ success }}
-            </div>
-            <div v-if="error" class="error-message">
-                {{ error }}
-            </div>
-            <div class="container">
-                <div id="map-container"></div>
-            </div>
-        </div>
+  <div class="map col-md-8">
+    <div>
+      <div v-if="success" class="success-message">
+        {{ success }}
+      </div>
+      <div v-if="error" class="error-message">
+        {{ error }}
+      </div>
+      <div class="container">
+        <div id="map-container"></div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    import axios from "axios";
-    import "leaflet/dist/leaflet.css";
-    import L from "leaflet";
-    // import mapquest from "mapquest";
-    import icon from "leaflet/dist/images/marker-icon.png";
-    import iconShadow from "leaflet/dist/images/marker-shadow.png";
-    import {eventBus} from "../main";
+import axios from "axios";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+// import mapquest from "mapquest";
+// import icon from "leaflet/dist/images/marker-icon.png";
+// import iconShadow from "leaflet/dist/images/marker-shadow.png";
+import { eventBus } from "../main";
 
-    export default {
-        name: "Map",
-        components: {},
-        props: {
-            type_id: Number,
-            neighbor_id: Number,
-            neighbor: String,
-            startdate: String,
-            enddate : String,
-        },
-        data() {
-            return {
-                map: undefined,
-                error: "",
-                success: "",
-                crimes: [],
-                center: [-71.1097, 42.3736],
-            };
-        },
-        created: function () {
-            eventBus.$on("changeType-success", () => {
-                this.getCrimes();
-            });
-
-            eventBus.$on("changeNeighbor-success", () => {
-                this.getCrimes();
-            });
-
-            eventBus.$on("changeStartDate-success", () => {
-                this.getCrimes();
-            });
-
-            eventBus.$on("changeEndDate-success", () => {
-                this.getCrimes();
-            });
-        },
-        mounted: function () {
-            this.getCrimes();
-        },
-
-        methods: {
-            getCrimes: async function () {
-                let that = await this;
-                axios
-                    .get(
-                        `/api/crimes?type=${that.$props.type_id}&neigh=${that.$props.neighbor_id}&from_=${that.$props.startdate}&to_=${that.$props.enddate}`
-                    )
-                    .then((response) => {
-                        const crimes = response.data;
-                        return crimes;
-                    }).then((crimes) => {
-                        return crimes.map((crime) => {
-                            return {
-                                  type: "Feature",
-                                  properties: {
-                                    fileNumber: crime.fileNumber,
-                                    reportDate: crime.reportDate,
-                                    crimeType: crime.crimeType,
-                                    neighborhood: crime.neighborhood,
-                                    location: crime.location.split(' ').slice(1).join(' '),
-                                  },
-                                  geometry: {
-                                    type: "Point",
-                                    coordinates: [crime.longitude, crime.latitude],
-                                  },
-                                }
-                        })
-                }).then((processedCrimes) => {
-                    that.crimes = processedCrimes;
-                }).then(() => {
-                    that.setupLeafletMap(that.crimes);
-                });
-            },
-
-            // geoCodeAddress: async (location, fn) => {
-            //     return mapquest.geocode(
-            //         {
-            //             address: [location],
-            //             key: "gX72Jyp1H1l0fIoGb06xGY6yJ9h6KMN1",
-            //         },
-            //         function (err, result) {
-            //             fn([result.displayLatLng.lng, result.displayLatLng.lat]);
-            //         }
-            //     );
-            // },
-            onEachFeature: (feature, layer) => {
-                if (
-                    feature.properties &&
-                    feature.properties.crimeType &&
-                    feature.properties.neighborhood &&
-                    feature.properties.location
-                ) {
-                    layer.bindPopup(
-                        feature.properties.neighborhood +
-                        ", " +
-                        feature.properties.crimeType +
-                        ", " +
-                        feature.properties.location
-                    );
-                    layer.on("mouseover", () => {
-                        layer.openPopup();
-                    });
-                    layer.on("mouseout", () => {
-                        layer.closePopup();
-                    });
-                }
-            },
-            setupLeafletMap: function (crimes) {
-                let mapDiv = this.map;
-                // var container = L.DomUtil.get('map-container');
-                //   if(container != null){
-                //   container._leaflet_id = null;
-                //   }
-                if (mapDiv !== undefined && mapDiv !== null) {
-                    mapDiv.remove();
-                }
-                mapDiv = L.map("map-container").setView(
-                    [42.37369292702129, -71.11050691418987],
-                    13
-                );
-                let DefaultIcon = L.icon({
-                    iconUrl: icon,
-                    shadowUrl: iconShadow,
-                });
-
-                L.Marker.prototype.options.icon = DefaultIcon;
-                L.tileLayer(
-                    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-                    {
-                        attribution: "© OpenStreetMap",
-                        maxZoom: 18,
-                        id: "mapbox/streets-v11",
-                        accessToken:
-                            "pk.eyJ1Ijoic3Zpc2h3YWIiLCJhIjoiY2tocGViOTF4MGl3ZTJzcDY5a2x0cDg4bSJ9.zu75vUX-kDvcReosDGWP1w",
-                    }
-                ).addTo(mapDiv);
-                let data = {
-                    type: "FeatureCollection",
-                    features: crimes,
-                };
-                let jsonData = JSON.stringify(data);
-                L.geoJson(JSON.parse(jsonData), {
-                    onEachFeature: this.onEachFeature,
-                }).addTo(mapDiv);
-                this.map = mapDiv;
-            },
-
-            // resolveCrimePromise: async function (crime) {
-            //     let coord;
-            //     await this.geoCodeAddress(crime.location, function (result) {
-            //         coord = result;
-            //     });
-            //
-            //     // wait for coord to to be assigned to result in callback
-            //     // before i return object below that references coord in
-            //     // the geometry.coordinates field
-            //     return {
-            //         type: "Feature",
-            //         properties: {
-            //             fileNumber: crime.fileNumber,
-            //             reportDate: crime.reportDate,
-            //             crimeType: crime.crimeType,
-            //             neighborhood: crime.neighborhood,
-            //             location: crime.location.split(' ').slice(1).join(' '),
-            //         },
-            //         geometry: {
-            //             type: "Point",
-            //             coordinates: coord,
-            //         },
-            //     };
-            // },
-
-
-            // loadCrimes: async function () {
-            //   let crimeData;
-            //   axios
-            //     .get("/api/crimes/")
-            //     .then((response) => {
-            //       crimeData = response.data;
-            //     })
-            //     .then(() => {
-            //       let promises = crimeData.map((crime) => {
-            //         return this.resolveCrimePromise(crime);
-            //       });
-            //       Promise.all(promises).then((results) => {
-            //         crimeData = results;
-            //       });
-            //     });
-            // },
-
-            // newLoadCrimes: async function () {
-            //   let that = await this;
-            //   let res = await axios
-            //     .get(
-            //       `/api/crimes?type=${that.$props.type_id}&neigh=${that.$props.neighbor_id}`
-            //     )
-            //     .then((response) => {
-            //       return response.data;
-            //     })
-            //     .then((crimes) => {
-            //       const KEY = "2PiwKOE0ZPWlhGEJjVghZmpmDWoYYGiJ";
-            //       const promises = crimes.map((crime) => {
-            //         return axios
-            //           .get(
-            //             `https://www.mapquestapi.com/geocoding/v1/address?key=${KEY}&location=${crime.location}`
-            //           )
-            //           .then((response) => {
-            //             return response.data.results[0].locations[0].displayLatLng;
-            //           })
-            //           .then((location) => {
-            //             return {
-            //               type: "Feature",
-            //               properties: {
-            //                 fileNumber: crime.fileNumber,
-            //                 reportDate: crime.reportDate,
-            //                 crimeType: crime.crimeType,
-            //                 neighborhood: crime.neighborhood,
-            //                 location: crime.location.split(' ').slice(1).join(' '),
-            //               },
-            //               geometry: {
-            //                 type: "Point",
-            //                 coordinates: [location.lng, location.lat],
-            //               },
-            //             };
-            //           });
-            //       });
-            //       Promise.all(promises)
-            //         .then((Data) => {
-            //           that.crimes = Data;
-            //         })
-            //         .then(() => {
-            //           this.setupLeafletMap(that.crimes);
-            //         });
-            //     })
-            //     .catch((error) => {
-            //       alert(error);
-            //     });
-            //   return res;
-            // },
-
-            clearMessages: function () {
-                setInterval(() => {
-                    this.success = "";
-                    this.error = "";
-                }, 5000);
-            },
-        },
+export default {
+  name: "Map",
+  components: {},
+  props: {
+    type_id: Number,
+    neighbor_id: Number,
+    neighbor: String,
+    startdate: String,
+    enddate: String,
+    userId: String,
+  },
+  data() {
+    return {
+      map: undefined,
+      error: "",
+      success: "",
+      crimes: [],
+      center: [-71.1097, 42.3736],
+      followedNeighborhoods: [],
     };
+  },
+  created: function () {
+    eventBus.$on("changeType-success", () => {
+      this.getFollowedNeighborhoods();
+    });
+
+    eventBus.$on("changeNeighbor-success", () => {
+      this.getFollowedNeighborhoods();
+    });
+
+    eventBus.$on("changeStartDate-success", () => {
+      this.getFollowedNeighborhoods();
+    });
+
+    eventBus.$on("changeEndDate-success", () => {
+      this.getFollowedNeighborhoods();
+    });
+
+    eventBus.$on("follow-neighbor-success", () => {
+      this.getFollowedNeighborhoods();
+    });
+
+    eventBus.$on("unfollow-neighbor-success", () => {
+      this.getFollowedNeighborhoods();
+    });
+  },
+  mounted: function () {
+    this.getFollowedNeighborhoods();
+  },
+
+  methods: {
+    getCrimes: async function () {
+      let that = await this;
+      axios
+        .get(
+          `/api/crimes?type=${that.$props.type_id}&neigh=${that.$props.neighbor_id}&from_=${that.$props.startdate}&to_=${that.$props.enddate}`
+        )
+        .then((response) => {
+          const crimes = response.data;
+          return crimes;
+        })
+        .then((crimes) => {
+          return crimes.map((crime) => {
+            return {
+              type: "Feature",
+              properties: {
+                fileNumber: crime.fileNumber,
+                reportDate: crime.reportDate,
+                crimeType: crime.crimeType,
+                neighborhood: crime.neighborhood,
+                location: crime.location.split(" ").slice(1).join(" "),
+              },
+              geometry: {
+                type: "Point",
+                coordinates: [crime.longitude, crime.latitude],
+              },
+            };
+          });
+        })
+        .then((processedCrimes) => {
+          that.crimes = processedCrimes;
+        })
+        .then(() => {
+          that.setupLeafletMap(that.crimes);
+        });
+    },
+    onEachFeature: (feature, layer) => {
+      if (
+        feature.properties &&
+        feature.properties.crimeType &&
+        feature.properties.neighborhood &&
+        feature.properties.location
+      ) {
+        layer.bindPopup(
+          feature.properties.neighborhood +
+            ", " +
+            feature.properties.crimeType +
+            ", " +
+            feature.properties.location
+        );
+        layer.on("mouseover", () => {
+          layer.openPopup();
+        });
+        layer.on("mouseout", () => {
+          layer.closePopup();
+        });
+      }
+    },
+    setupLeafletMap: async function (crimes) {
+      let mapDiv = this.map;
+      if (mapDiv !== undefined && mapDiv !== null) {
+        mapDiv.remove();
+      }
+      mapDiv = L.map("map-container").setView(
+        [42.37369292702129, -71.11050691418987],
+        13
+      );
+      // let DefaultIcon = L.icon({
+      //   iconUrl: icon,
+      //   shadowUrl: iconShadow,
+      // });
+
+      // L.Marker.prototype.options.icon = DefaultIcon;
+      L.tileLayer(
+        "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+        {
+          attribution: "© OpenStreetMap",
+          maxZoom: 18,
+          id: "mapbox/streets-v11",
+          accessToken:
+            "pk.eyJ1Ijoic3Zpc2h3YWIiLCJhIjoiY2tocGViOTF4MGl3ZTJzcDY5a2x0cDg4bSJ9.zu75vUX-kDvcReosDGWP1w",
+        }
+      ).addTo(mapDiv);
+      let data = {
+        type: "FeatureCollection",
+        features: crimes,
+      };
+      let jsonData = JSON.stringify(data);
+
+      let that = await this;
+      L.geoJson(JSON.parse(jsonData), {
+        onEachFeature: this.onEachFeature,
+        pointToLayer: function (feature, latlng) {
+          // orange icon
+          let markerHtmlStylesOrange = `
+                background-color: ${"#F68A37"};
+                width: 1.5rem;
+                height: 1.5rem;
+                display: block;
+                left: -.8rem;
+                top: -1.5rem;
+                position: relative;
+                border-radius: 3rem 3rem 0;
+                transform: rotate(45deg);
+                border: 1px solid #FFFFFF`;
+          let orangeIcon = L.divIcon({
+            className: "",
+            iconAnchor: [0, 24],
+            labelAnchor: [-6, 0],
+            popupAnchor: [0, -36],
+            html: `<span style="${markerHtmlStylesOrange}" />`,
+          });
+
+          // blue icon
+          let markerHtmlStylesBlue = `
+                background-color: ${"#236AB9"};
+                width: 1.5rem;
+                height: 1.5rem;
+                display: block;
+                left: -.8rem;
+                top: -1.5rem;
+                position: relative;
+                border-radius: 3rem 3rem 0;
+                transform: rotate(45deg);
+                border: 1px solid #FFFFFF`;
+          let blueIcon = L.divIcon({
+            className: "",
+            iconAnchor: [0, 24],
+            labelAnchor: [-6, 0],
+            popupAnchor: [0, -36],
+            html: `<span style="${markerHtmlStylesBlue}" />`,
+          });
+
+          if (
+            that.followedNeighborhoods.includes(feature.properties.neighborhood)
+          ) {
+            return new L.Marker(latlng, { icon: orangeIcon });
+          } else {
+            return new L.Marker(latlng, { icon: blueIcon });
+          }
+        },
+      }).addTo(mapDiv);
+      this.map = mapDiv;
+    },
+
+    getFollowedNeighborhoods: async function () {
+      let that = await this;
+      if (that.$props.userId) {
+        axios.get(`/api/feeds/user?id=${that.$props.userId}`).then((res) => {
+          let neighborhoodIds = [...res.data];
+          that.followedNeighborhoods = [];
+          neighborhoodIds.map((n) => {
+            axios
+              .get(`/api/neighborhoods/${n.neighborhoodId}`)
+              .then((res2) => {
+                that.followedNeighborhoods.push(res2.data.neighborhood);
+              })
+              .then(() => {
+                that.getCrimes();
+              });
+          });
+        });
+      } else {
+        that.followedNeighborhoods = [];
+        that.getCrimes();
+      }
+    },
+
+    clearMessages: function () {
+      setInterval(() => {
+        this.success = "";
+        this.error = "";
+      }, 5000);
+    },
+  },
+};
 </script>
 
 <style scoped>
-    #map-container {
-        overflow: hidden;
-        padding-bottom: 56.25%;
-        position: relative;
-        height: 0;
-        margin: 5%;
-    }
+#map-container {
+  overflow: hidden;
+  padding-bottom: 56.25%;
+  position: relative;
+  height: 0;
+  margin: 5%;
+}
 </style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -268,6 +268,7 @@ export default {
     },
 
     follow: function () {
+      console.log("in follow");
       if (this.userId !== "" && this.neighbor_id !== 0) {
         const bodyContent = {
           userId: this.userId,
@@ -275,7 +276,7 @@ export default {
         };
         axios.post("/api/feeds", bodyContent).then(() => {
           this.loadFollowing();
-          eventBus.$emit("follow-neighbor-succes", true);
+          eventBus.$emit("follow-neighbor-success", true);
         });
       }
     },
@@ -289,7 +290,7 @@ export default {
           })
           .then(() => {
             this.loadFollowing();
-            eventBus.$emit("unfollow-neighbor-succes", true);
+            eventBus.$emit("unfollow-neighbor-success", true);
           });
       }
     },
